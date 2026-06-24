bc15d9c95b13437da8d6d9f4878da0c6b060fe64
rakhaPC <rakha.guevara2505@gmail.com>
2026-06-24 13:02:12 +0700

    feat(frontend): add 404 catch-all page

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import AboutPage from '../pages/AboutPage'
+import NotFoundPage from '../pages/NotFoundPage'
 
 interface AppRoutesProps {
   onSignupClick: () => void
@@ -11,6 +12,7 @@ export default function AppRoutes({ onSignupClick }: AppRoutesProps) {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/tentang-kami" element={<AboutPage onSignupClick={onSignupClick} />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
 }
